6178e9eb5da7df862b68d435765c4bbca28f33b1
Adilson MANZOLI Junior <AManzoli@ttinteractive.com>
2020-03-11 15:47:14 -0300

    Small correct

--- src/module/application.instance.ts
+++ src/module/application.instance.ts
@@ -1,34 +1,19 @@
-// import { ModuleInstance } from './module.instance';
-// import { Injector } from '../provider/injector';
-// import { DefinedProvider } from '../provider/providers';
 import { ModuleStore } from './module.store';
-import { Interceptor } from '../interception/interceptor';
+import { ModuleInstance } from './module.instance';
 
 export class ApplicationInstance {
+
+    protected module: ModuleInstance
 
     public constructor(
         cls: Object
     ) {
-        //(<any>cls).__application__ = this;
-        
-        let isApplication = Reflect.getOwnMetadata('application:is', cls);
+        const isApplication = Reflect.getOwnMetadata('application:is', cls);
         if (!isApplication) {
             throw 'Invalid application! ' + cls.toString();
         }
 
-        //let rootContainer = <IProviderContainer>{};
-        //let injector = new Injector(rootContainer);
-        //this.defineCustomData(rootContainer, injector);
         const store = new ModuleStore();
-        store.getInstance(cls);
-        //new ModuleInstance(rootContainer, injector, cls);
+        this.module = store.getInstance(cls);
     }
-
-    // protected defineCustomData(container: IProviderContainer, injector: Injector): void {
-    //     if (!container.providers) {
-    //         container.providers = [];
-    //     }
-        
-    //     container.providers.push(new DefinedProvider(Injector, injector));
-    // }
 }
